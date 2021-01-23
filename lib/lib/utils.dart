441ccceaceae3f25c543ee495f58f201db785a9a
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2021-01-24 01:48:01 +0900

    Change hour format 1-12 to 0-11

--- lib/lib/utils.dart
+++ lib/lib/utils.dart
@@ -8,19 +8,24 @@ String padNumber(int num) {
   return "$num";
 }
 
+/// Get the hour for print
+int getHourForPrint(int h, bool is24Hr) {
+  if (is24Hr) {
+    return h;
+  }
+  if (h == 0) {
+    return 12;
+  }
+  return h;
+}
+
 /// Get the hour either in 12Hr or 24Hr format
 int getHours(int h, String a, bool is24Hr) {
   if (is24Hr) {
     return h;
   }
   if (a == 'am') {
-    if (h == 12) {
-      return 0;
-    }
     return h;
-  }
-  if (h == 12) {
-    return 12;
   }
   return h + 12;
 }
